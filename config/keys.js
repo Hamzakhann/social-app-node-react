cf2ef40e6f6c02d5ef49c2d56435c0b95a20159f
Hamzakhann <hamzakhann66@gmail.com>
2018-08-06 14:56:42 +0500

    add register and sign in route

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI :'mongodb://hamza:hamza1411@ds215388.mlab.com:15388/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const app = express();
+// DB Config
+const db  = require('./config/keys').mongoURI;
+// connect to mongo db with mongoose
+mongoose.connect(db).then(()=> console.log('Mongo db is connnected to server')).catch(err => console.log(err));
+
+app.get('/' , (req , res)=>{
+  res.send('Hello')
+});
+
+// Use  Routes
+app.use('/api/users' , users);
+app.use('/api/posts' , posts);
+app.use('/api/profile' , profile);
+const port = process.env.PORT || 5000;
+
+app.listen(port , ()=>{
+  console.log(`Server is running on port ${port}`)
+});
