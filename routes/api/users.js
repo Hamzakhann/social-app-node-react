79dd8d71fcb1dafb0a117286b341019c38c23128
Hamzakhann <hamzakhann66@gmail.com>
2018-08-06 14:57:28 +0500

    add sign in route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+// load user model
+const User = require('../../models/User');
 
+//@route GET api/users/test
+//@desc  Test users route
+//@access public
 router.get('/test' , (req , res) => res.json({msg : 'user works'}));
+
+
+
+//@route POST api/users/registeration
+//@desc  Register user
+//@access public
+router.post('/register' , (req , res)=>{
+  User.findOne({email : req.body.email}).then((user)=>{
+    if(user){
+      return res.status(400).json({email : 'email already exist'});
+    }else {
+      const avatar = gravatar.url(req.body.email , {
+        s : '200', //size
+        r : 'pg', //Rating
+        d : 'mm' //Default
+      });
+      const newUser = new User({
+        name : req.body.name,
+        email : req.body.email,
+        avatar,
+        password : req.body.password
+      })
+    
+      bcrypt.genSalt(10 , (err , salt)=>{
+        bcrypt.hash(newUser.password , salt , (err , hash)=>{
+          if(err) throw err;
+          newUser.password = hash;
+          newUser.save()
+          .then((user)=> res.json(user))
+          .catch(err => console.log(err))
+        });
+
+      });
+
+
+    }
+
+  })
+});
+
+
+
+//@route POST api/users/login
+//@desc  login user and return jwt token to the user
+//@access public
+
+router.post('/login' , (req , res) =>{
+  const email = req.body.email;
+  const password = req.body.password
+  console .log(email , password)
+  //find user by email
+  User.findOne({email})
+  .then((user)=>{
+    //check for user
+    if(!user){
+      return res.status(404).json({email : 'User not found'})
+    }
+    //check password
+    bcrypt.compare(password , user.password)
+    .then((isMatch)=>{
+      if(isMatch){
+        res.json({msg : 'success'})
+      }else{
+        return res.status(400).json({password : 'password incorrect'})
+      }
+    });
+
+
+  });
+});
 
 module.exports = router;
