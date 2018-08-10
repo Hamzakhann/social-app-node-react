e3e71da5dc672b08340bbaafaadd5323860e193b
Hamzakhann <hamzakhann66@gmail.com>
2018-08-10 12:56:57 +0500

    add registration functionality with redux

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,8 +1,10 @@
 import {GET_ERRORS} from '../actions/types';
-const initialState = {}
+const initialState = {};
+
 
 export default function(state = initialState , action){
   switch(action.type){
+
     default:
      return state;
   }
