dbec1e941a92007720f42c00ba26ade7e9b85762
Hamzakhann <hamzakhann66@gmail.com>
2018-08-10 14:36:26 +0500

    set current user with jwt token

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,6 +1,7 @@
-import {GET_ERRORS} from './types'
+import {GET_ERRORS , SET_CURRENT_USER} from './types'
 import axios from 'axios';
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken'
 //Register user 
 export const registerUser = (userData , history) => dispatch =>{
   axios.post('/api/users/register' , userData)
@@ -13,3 +14,36 @@ export const registerUser = (userData , history) => dispatch =>{
   );
 
 };
+
+
+
+//Login user GET user token
+
+export const loginUser = (userData) => dispatch =>{
+  axios.post('/api/users/login' , userData)
+  .then(res => {
+    //save to local storage
+    const {token} = res.data
+    //set token to ls
+    localStorage.setItem('jwtToken' , token);
+    //set token to auth header
+    setAuthToken(token)
+    //decode token to get user data
+    const decoded = jwt_decode(token);
+    //set current user
+    dispatch(setCurrentUser(decoded))
+  }).catch(err => {
+    dispatch({
+      type : GET_ERRORS,
+      payload : err.response.data
+    })
+  });
+};
+
+//set logged in user
+export const setCurrentUser = (decoded) =>{
+  return {
+    type : SET_CURRENT_USER,
+    payload : decoded
+  }
+}
